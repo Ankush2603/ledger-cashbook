ade862f5cf81c53a560c04a0ce168afde0468b90
ankush <ankush2020kedia@gmail.com>
2025-10-03 11:21:46 +0530

    intialized vite and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      borderColor: {
+        border: '#E5E7EB', // Example custom color - replace with your value
+      },
+    },
+  },
+  plugins: [],
+};
